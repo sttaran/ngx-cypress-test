024d25a1f2ca0839bf6ea1b2cc095460691bbb06
Santiago <alexsa.beauty@gmail.com>
2023-11-27 20:13:06 +0200

    Installed cypress in new branch and made basic configuration

--- cypress/e2e/steps/steps.spec.js
+++ cypress/e2e/steps/steps.spec.js
@@ -4,7 +4,7 @@ describe('Steps page', ()=>{
   })
 
 
-  it('should show valid title on each step', () => {
+  it('Should show valid title on each step', () => {
 
     const containerSelector = 'nb-card-body nb-stepper.horizontal[orientation="horizontal"]'
 
@@ -19,23 +19,5 @@ describe('Steps page', ()=>{
 
     cy.get(`${containerSelector} h3`).should('have.text', "Step content #4")
     cy.get(`${containerSelector}`).contains('next').should('be.disabled')
-  });
-
-  it('should show valid title on each step (with aliases)', () => {
-    cy.get('nb-card-body nb-stepper.horizontal[orientation="horizontal"]').as('container')
-    cy.get('@container').find('h3').as('stepperTitle')
-    cy.get('@container').contains('next').as('nextButton')
-
-    cy.get('@stepperTitle').should('have.text', "Step content #1")
-    cy.get('@nextButton').click()
-
-    cy.get('@stepperTitle').should('have.text', "Step content #2")
-    cy.get('@nextButton').click()
-
-    cy.get('@stepperTitle').should('have.text', "Step content #3")
-    cy.get('@nextButton').click()
-
-    cy.get('@stepperTitle').should('have.text', "Step content #4")
-    cy.get('@nextButton').should('be.disabled')
   });
 })
