a1d21b6d9039c1a45f7d56fb2ca7a3ee22747889
unknown <elena.zhorova@gmail.com>
2023-12-01 01:33:41 +0200

    New branch with task cypress-querities-actions-V.2

--- cypress/e2e/steps/DialogTest2.cy.js
+++ cypress/e2e/steps/DialogTest2.cy.js
@@ -7,5 +7,33 @@ describe("modal-overlays/dialog page", () => {
     cy.get('nb-card.form-input-card').find('button.status-primary').each(($element)=>{
       cy.wrap($element).click()
     })
-    });
+
+    // Чекати відображення модального вікна
+    cy.wait(500)
+    // Перевірка, що модальне вікно показане
+    cy.get('nb-card.form-input-card').should('be.visible');
+
+    // Перевірка тексту заголовка модального вікна
+    cy.get('.cdk-overlay-container nb-dialog-container nb-card-header').should('have.text', 'Enter your name');
+
+    // Перевірка наявності інпута
+    cy.get('nb-card-body input[placeholder="Name"]').should('exist');
+
+    // // Перевірка наявності кнопки 'Submit'
+    // cy.get('#cdk-overlay-1 button.cancel.appearance-filled.size-medium.status-danger.shape-rectangle.transitions')
+    //   .should('be.visible');
+    //
+    // // Перевірка наявності кнопки 'Cancel'
+    // cy.get('#cdk-overlay-1 button.cancel.appearance-filled.size-medium.status-danger.shape-rectangle.transitions')
+    //   .should('be.visible');
+
+    // Перевірка наявності кнопки 'Submit'
+    cy.get('.cdk-overlay-container nb-dialog-container button.appearance-filled.size-medium.status-success.shape-rectangle.transitions')
+      .should('be.visible');
+
+    // Перевірка наявності кнопки 'Cancel'
+    cy.get('.cdk-overlay-container nb-dialog-container button.cancel.appearance-filled.size-medium.status-danger.shape-rectangle.transitions')
+      .should('be.visible');
   });
+});
+
